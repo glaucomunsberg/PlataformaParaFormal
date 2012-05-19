700ed4f277d966e4c540834742391b7d8257bef8
Glauco Roberto Munsberg <glaucomunsberg@gmail.com>
2012-05-19 16:08:34 -0300

    Versão_0.1.9.5

--- _documentacao/about_plataforma/atualizarPlataforma.sh
+++ _documentacao/about_plataforma/atualizarPlataforma.sh
@@ -7,8 +7,9 @@ echo "-Atualizando com gitHub...                -"
 git reset --hard HEAD
 git pull origin master
 echo "-------------------------------------------"
-echo "Etapa 1 finalizada! Enter para finalizar  -"
+echo "Etapa 1 finalizada! Enter para continuar  -"
 read algo
+clear
 echo "-------------------------------------------"
 echo "-Programa de atualização da plataforma... -"
 echo "-Etapa 2                                  -"
@@ -24,9 +25,17 @@ if [ $operacao != "N" ]; then
 	echo "dropdb aurora" | sudo su - postgres
 	echo "createdb aurora" | sudo su - postgres
 	echo "\i /var/www/PlataformaDoParaformal/_documentacao/dumps/Aurora.sql" | sudo su postgres -c "psql aurora"
-	echo "Etapa 2 finalizada! Enter para finalizar  -"
+	echo "Etapa 2 finalizada! Enter para continuar  -"
 	read algo
 	else
-	echo "Etapa 2 abortada! Enter para finalizar    -"
+	echo "Etapa 2 abortada! Enter para continuar    -"
 	read algo
 fi
+clear
+echo "-------------------------------------------"
+echo "-Programa de atualização da plataforma... -"
+echo "-Etapa 3                                  -"
+echo "-Permissão de arquivos                    -"
+sudo chmod +x /_documentacao/about_plataforma/atualizarPlataforma.sh
+echo "Etapa 3 finalizada! Enter para sair...    -"
+read a
