b597f87859470c13177f0c4a6bf424a1c38b9f35
Glauco Roberto Munsberg <glaucomunsberg@gmail.com>
2012-07-30 14:33:59 -0300

    Versão_0.3.3.2

---
+++ system/logs/log-2012-07-30.php
@@ -0,0 +1,48 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2012-07-30 13:51:57 --> Config Class Initialized
+DEBUG - 2012-07-30 13:51:57 --> Hooks Class Initialized
+DEBUG - 2012-07-30 13:51:57 --> URI Class Initialized
+DEBUG - 2012-07-30 13:51:57 --> No URI present. Default controller set.
+DEBUG - 2012-07-30 13:51:57 --> Router Class Initialized
+DEBUG - 2012-07-30 13:51:57 --> Output Class Initialized
+DEBUG - 2012-07-30 13:51:57 --> Input Class Initialized
+DEBUG - 2012-07-30 13:51:57 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-07-30 13:51:57 --> Language Class Initialized
+DEBUG - 2012-07-30 13:51:57 --> Loader Class Initialized
+DEBUG - 2012-07-30 13:51:57 --> Helper loaded: url_helper
+DEBUG - 2012-07-30 13:51:57 --> Helper loaded: lang_helper
+DEBUG - 2012-07-30 13:51:57 --> Helper loaded: formaux_helper
+DEBUG - 2012-07-30 13:51:57 --> Helper loaded: form_helper
+DEBUG - 2012-07-30 13:51:57 --> Helper loaded: util_helper
+DEBUG - 2012-07-30 13:51:57 --> Helper loaded: toolbar_helper
+DEBUG - 2012-07-30 13:51:57 --> Helper loaded: tabpanel_helper
+DEBUG - 2012-07-30 13:51:57 --> Helper loaded: gridpanel_helper
+DEBUG - 2012-07-30 13:51:57 --> Helper loaded: jqgridpanel_helper
+DEBUG - 2012-07-30 13:51:57 --> Helper loaded: editorgridpanel_helper
+DEBUG - 2012-07-30 13:51:57 --> Helper loaded: download_helper
+DEBUG - 2012-07-30 13:51:57 --> Helper loaded: inflector_helper
+DEBUG - 2012-07-30 13:51:57 --> Helper loaded: string_helper
+DEBUG - 2012-07-30 13:51:57 --> Language file loaded: language/pt-br/system_lang.php
+DEBUG - 2012-07-30 13:51:57 --> Language file loaded: language/pt-br/login_lang.php
+DEBUG - 2012-07-30 13:51:57 --> Database Driver Class Initialized
+DEBUG - 2012-07-30 13:51:58 --> Encrypt Class Initialized
+DEBUG - 2012-07-30 13:51:58 --> Unit Testing Class Initialized
+DEBUG - 2012-07-30 13:51:58 --> Session Class Initialized
+DEBUG - 2012-07-30 13:51:58 --> Encrypt class already loaded. Second attempt ignored.
+DEBUG - 2012-07-30 13:51:58 --> Session routines successfully run
+DEBUG - 2012-07-30 13:51:58 --> Controller Class Initialized
+DEBUG - 2012-07-30 13:51:58 --> Model Class Initialized
+DEBUG - 2012-07-30 13:51:58 --> Model Class Initialized
+DEBUG - 2012-07-30 13:51:58 --> Model Class Initialized
+DEBUG - 2012-07-30 13:51:58 --> Model Class Initialized
+DEBUG - 2012-07-30 13:51:58 --> Model Class Initialized
+DEBUG - 2012-07-30 13:51:58 --> Model Class Initialized
+ERROR - 2012-07-30 13:51:58 --> Severity: Notice  --> Undefined index: 92c29c1ac4d85b45639f741599c24cd7 /home/glaucoroberto/Projetos/PlataformaDoParaformal/autenticacao/controllers/login.php 21
+ERROR - 2012-07-30 13:51:58 --> Severity: Notice  --> Undefined index: tema /home/glaucoroberto/Projetos/PlataformaDoParaformal/static/_views/headerScripts.php 8
+DEBUG - 2012-07-30 13:51:58 --> File loaded: autenticacao/views/../../static/_views/headerScripts.php
+DEBUG - 2012-07-30 13:51:58 --> File loaded: autenticacao/views/../../static/_views/headerLoginView.php
+DEBUG - 2012-07-30 13:51:58 --> File loaded: autenticacao/views/../../static/_views/footerLoginView.php
+DEBUG - 2012-07-30 13:51:58 --> File loaded: autenticacao/views/loginView.php
+DEBUG - 2012-07-30 13:51:58 --> Final output sent to browser
+DEBUG - 2012-07-30 13:51:58 --> Total execution time: 1.4943
